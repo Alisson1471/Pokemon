3ee3a927c11fc28593f1033ef3fc3db3daf4175d
Alisson1471 <alissonalvesdossantosoliveira@gmail.com>
2025-03-19 15:23:38 -0300

    Configurando o Dockerfile para deploy e adicionando o swagger no projeto

--- src/main/java/Pokemon/PokemonApplication.java
+++ src/main/java/Pokemon/PokemonApplication.java
@@ -1,5 +1,6 @@
 package Pokemon;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -7,6 +8,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 
 @EnableFeignClients
 @SpringBootApplication
+@OpenAPIDefinition
 public class PokemonApplication {
 
 	public static void main(String[] args) {
